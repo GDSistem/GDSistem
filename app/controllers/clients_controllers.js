55b9b1a896d2debe911687418dd8c1f2c7d65ea8
FrankC-12 <facaicedoh@gmail.com>
2025-05-07 11:39:27 -0400

    [ADD] Agregue toda la logica de los clientes

---
+++ app/controllers/clients_controllers.js
@@ -0,0 +1,42 @@
+const {obtenerCliente} = require('../models/clients_models'); // Asegúrate de que la ruta sea correcta
+
+// Controlador para obtener la empresa y sus sucursales
+// Payload que espera recibir
+// {
+//   "codDocumento": "documento"
+// }
+const getCliente = async (req, res) => {
+    const { codDocumento } = req.body;
+  
+    // Validación básica
+    if (!codDocumento || typeof codDocumento !== 'string') {
+      return res.status(400).json({
+        success: false,
+        message: 'El campo codDocumento es requerido y debe ser una cadena de texto.'
+      });
+    }
+  
+    try {
+      const data = await obtenerCliente(codDocumento);
+  
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'No se encontró ningún documento con el código proporcionado.'
+        });
+      }
+  
+      res.status(200).json({
+        success: true,
+        data
+      });
+    } catch (error) {
+      console.error('Error al obtener documento:', error);
+      res.status(500).json({
+        success: false,
+        message: 'Error interno del servidor al obtener el documento.'
+      });
+    }
+  };
+  
+  module.exports = { getCliente };
---
+++ app/routes/clients_routes.js
@@ -0,0 +1,8 @@
+// routes/metodos.routes.js
+const express = require('express');
+const router = express.Router();
+const { getCliente } = require('../controllers/clients_controllers');
+
+router.post('/cliente', getCliente);
+
+module.exports = router;
